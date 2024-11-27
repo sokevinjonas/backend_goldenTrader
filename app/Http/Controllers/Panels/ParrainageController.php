98c8226d0d41c3252fd921f91bff4e514ad4d176
sokevinjonas <kjonasdevpro@gmail.com>
2024-11-27 22:49:54 +0000

    creation des controllers Abonnements, Authenfication, Parrainage, Publication et Utilisateur

---
+++ app/Http/Controllers/Panels/ParrainageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Panels;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ParrainageController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Panels/PublicationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Panels;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PublicationController extends Controller
+{
+    //
+}
